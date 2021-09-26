07379a8f5297612fb12928dc794a2d30ac8ca518
Conor Garry <conor.garry@marinosoftware.com>
2021-09-26 08:30:58 +0100

    Add 19. Remove Nth Node From End of List solution

---
+++ src/main/kotlin/RemoveNthNodeFromEndOfList.kt
@@ -0,0 +1,44 @@
+/**
+ * 19. Remove Nth Node From End of List.
+ *
+ * Given the head of a linked list, remove the nth
+ * node from the end of the list and return its head.
+ */
+fun main() {
+    println(removeNthFromEnd(buildLinkedList(1, 2), 1))
+    println(removeNthFromEndRec(buildLinkedList(1, 2, 3, 4, 5), 2))
+}
+
+fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+    var fast: ListNode? = head ?: return null
+    var slow = fast
+
+    for (i in 0 until n) { // Fast pointer head start.
+        fast = fast?.next
+    }
+    // Boundary case, returning first node.
+    if (fast == null) {
+        return head.next
+    }
+
+    while (fast?.next != null) {
+        fast = fast.next
+        slow = slow?.next
+    }
+
+    // Unlink
+    slow?.next = slow?.next?.next
+    return head
+}
+
+// Recursion with auxiliary counter.
+fun removeNthFromEndRec(head: ListNode?, n: Int): ListNode? {
+    var i = 0
+    fun remove(head: ListNode?, n: Int): ListNode? {
+        if (head == null) return null
+        head.next = remove(head.next, n)
+        i++
+        return if (i == n) head.next else head
+    }
+    return remove(head, n)
+}
